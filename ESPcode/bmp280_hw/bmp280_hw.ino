05351e980abf44eba6266e45aadb0efd15037358
MaciejZawartka <44232712+MaciejZawartka@users.noreply.github.com>
2020-01-05 22:00:23 +0100

    Pierwsza MQTT z odczytem temp/cis

--- ESPcode/bmp280_hw/bmp280_hw.ino
+++ ESPcode/bmp280_hw/bmp280_hw.ino
@@ -1,149 +1,151 @@
- /**********************************************
- Połączenie czujnika BMP280 do płytki HW 628 v1.1
- Odczyt danych z czujnka
- Polaczenie z siecią
- wysyłanie danych do clienta
-**********************************************/
+/***************************************************
+Nalezy sciagnac biblioteke:
+  Adafriut MQTT Library by Adafriut ver. 1.0.3
+Nalezy dodac biblioteke:
+  Plik>Preferencje>Dodatkowe biblioteki:
+    https://dl.espressif.com/dl/package_esp32_index.json    -> biblioteka uzywana dla dzialania programu na NODEMCU v3
+    https://arduino.esp8266.com/stable/package_esp8266com_index.json  -> biblioteka dla komunikacji MQTT
 
+
+
+MORE INFO:
+
+AUTHORS:    Kamil Walczyk
+            Maciej Zawartka
+DESC:       Program na plytke HW-628 v1.1  (NODEMCU v3)
+            Wspoldziala z czujnikiem BMP280. Odczyt temperatury oraz cisnienia. 
+            PODLACZENIE
+            NODE MCU -> BMP280
+            D1       -> SCL
+            D2       -> SDA
+            GND      -> GND
+            3v3      -> VCC, SDD, CSB
+****************************************************/
+//Deklaracja bibliotek
 #include <Wire.h>
 #include <SPI.h>
+#include <ESP8266WiFi.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
-#include <ESP8266WiFi.h> 
+#include <Adafruit_MQTT.h>
+#include "libs\Adafruit_MQTT_Client.h"
 
-#define BMP_SCK 13
-#define BMP_MISO 12
-#define BMP_MOSI 11 
-#define BMP_CS 10
-
+//******* SEKTOR CZUJNIKA ================
+float temp, pres;
+int poczekaj= 5000; 
 Adafruit_BMP280 bme; // I2C
-float temp,pres,altit;
-int ilosc_odczytow=0;
 
-// ====================== dane do sieci wi fi 
-String apiKey = "192.168.0.15";
-const char* server1 = "80";
+#define ID_URZ "bmp280e1";  //definicja id urzadzenia
 
-//=================================== dom    ==========
-//const char* ssid = "TP-LINK_832C7D";
-//const char* password = "90699443";
+/************************* WiFi Access Point *********************************/
 
-// ============== samsung =========
-//const char* ssid = "Kamilwifi";
-//const char* password = "11112222";
+#define WLAN_SSID       "UPC2501659" // enter your WiFi SSID
+#define WLAN_PASS       "wy2fHfsvJkyp" // this is your WiFi password
 
-// ============== mackowe =========
-const char* ssid = "UPC2501659";
-const char* password = "wy2fHfsvJkyp";
+/************************* Dane do polaczenia z Brokerem MQTT *********************************/
 
+#define MQTT_SERVER      "192.168.0.200" // Adress IP Brokera MQTT
+#define MQTT_SERVERPORT  1883            // Port do wyslanie
+#define MQTT_USERNAME    ""              // W przypadku uzywania autoryzacji
+#define MQTT_KEY         ""              // W przypadku uzywania autoryzacji
 
-// ===SOSNOWIEC===
-//const char* ssid = "UPC52173CC";
-//const char* password = "CsebYcwrm4zz";
+/************ Global State  ******************/
 
+//Tworzenie klasy clienta dla NodeMCU do polaczenia z serwerem
 WiFiClient client;
-WiFiServer server(80);
 
+//Tworzenie polaczenia klasy clienta MQTT przez podanie polaczenia WiFi i Brokera MQTT oraz danych logowania
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_SERVERPORT, MQTT_USERNAME, MQTT_KEY);
 
-//================= wspolczynniki kalibracyjne ============================
-float korekt_temp,korekt_pres,korekt_wys=1;
+/****************************** data stream publish/subscribe ***************************************/
 
-//===============================================================================
-//=============================================  SETUP  =========================
-//===============================================================================
+Adafruit_MQTT_Publish temp_stream = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME "b1/temp");
+Adafruit_MQTT_Publish pres_stream = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME "b1/pres");  
+/*************************** Kod ************************************/
+
+void MQTT_connect();
+
 void setup() {
-  
-  Serial.begin(9600);
+  Serial.begin(115200);
+  delay(10);
   Serial.println(F("BMP280 test"));
-
-  //=============== sprawdzenie czy jest podłączony czujnik ================
+  //Sprawdzenie czy jest podłączony czujnik 
   if (!bme.begin()) {  
-    Serial.println("Sprawdz połączenie czujnika!");
+    Serial.println("Sprawdz polaczenie czujnika!");
     while (1);
   }
- // WiFi.begin(ssid, password);
-  
-  Serial.println();
-  Serial.println();
+  // Laczenie do AP WiFi
+  Serial.println(); Serial.println();
   Serial.print("Connecting to ");
-  Serial.println(ssid);
-  
-  WiFi.begin(ssid, password);
-  
-  //=============== progres w łaczeniu sie z siecia=============
-  while (WiFi.status() != WL_CONNECTED) 
-  {
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  //============== polaczono z wi fi ==================
-  Serial.println("");
-  Serial.println(" Polaczono sie z siecia WiFi ");  
- //================== adres gdzie sa rzeczy ================
-  Serial.print("Use this URL to connect: ");
-  Serial.print("http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("/"); 
-  server.begin();
+  Serial.println();
+
+  Serial.println("WiFi polaczone");
+  Serial.println("Adres IP: "); Serial.println(WiFi.localIP());
+
+
+  
 }
 
-//================================================================================================
-//================================================================================================
+
+
+//********** WLASCIWY PROGRAM **********************
+uint32_t x=0;
 
 void loop() {
- 
-  //================== serwer ================
-    WiFiClient client = server.available();
-    if (!client) {
-      return;
-    }
-     
-    // Wait until the client sends some data
-    Serial.println("new client");
-    int timewate = 0;
-    while(!client.available()){
-      delay(1);
-      timewate = timewate +1;
-      if(timewate>1800)
-      {
-        Serial.println(">>> Client Timeout !");
-        client.stop();
-        return;
-      }
-    }
-  //===================== odswiezenie strony co odczyt - na komp - dziala/ telefon - nie dziala=====
-   client.print("<meta http-equiv='refresh' content='1'>");
-   client.println('\n');
+  // Sprawdzanie polaczenia z serwerem i ponowne polaczanie gdy wymagane  
+  MQTT_connect();
 
-  //================ zliczanie odczytow =================================
+  // ODCZYT WARTOSCI
+  float temp=bme.readTemperature();
+  float pres=bme.readPressure()/100;
+  // WYPISANIE NA KONSOLI  
+  Serial.print("Temperatura : ");
+  Serial.println(temp,DEC);
+  Serial.print("Cisnienie : ");
+  Serial.println(pres,DEC);
+  //PUBLIKOWANIE
+  temp_stream.publish(temp);
+  delay(100);
+  pres_stream.publish(pres);
+  //POCZEKAJ
+  delay(poczekaj);
   
-   ilosc_odczytow=ilosc_odczytow+1;
-    client.print((String)"jest to odczyt numer: "+ilosc_odczytow);
-   client.println('\n');
-  //=============== odczyt temperatury =================================
-    Serial.print("T=");
-    temp=bme.readTemperature();
-    Serial.print(temp);
-    Serial.print(" *C");
-    client.println((String)"temperatura: "+temp+" C");
-    client.println('\n');
-    
-  //=============== odczyt cisnienia ===================================
-    Serial.print(" P=");
-    pres=bme.readPressure()/100;
-    Serial.print(pres);
-    Serial.print(" hPa");
-    client.println((String)"cisnienie: "+pres+" hPa");
-    client.println('\n');
-    
-  //=============== odczyt wysokosci na podst. cisnienia odniesienia ===
-    Serial.print(" A= ");
-    altit=bme.readAltitude(1013.25);
-    Serial.print(altit); // this should be adjusted to your local forcase
-    Serial.println(" m");
-    client.println((String)"wysokosc: "+altit+" m");
-    client.println('\n');
-  
-    //przerwa 5s  
-    delay(1000);
+
+}
+
+
+
+
+//====== KONTROLA POLACZENIA ========
+// Funkcja do laczenia sie ( i odnawiania polaczenia) z Brokerem MQTT
+void MQTT_connect() {
+  int8_t ret;
+
+  // Wprawdz czy polaczone
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Laczenie z MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // 0 oznacza polaczenie udane
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Ponawianie proby za 5 sek...");
+       mqtt.disconnect();
+       delay(5000);  
+       retries--;
+       if (retries == 0) {
+         // uspij
+         while (1);
+       }
+  }
+  Serial.println("MQTT Polaczone!");
 }
